5ebea36efaba7b92e14e8c8bb9bfa57740a8186e
Ian De La Cruz <ridlc10@gmail.com>
2015-05-07 13:43:42 +0800

    Updated versions

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 // Database URL
 module.exports = {
-    // Uncomment to connect with MongoDB on Cloud
-    'url' : 'mongodb://feiochc:hate666!@kahana.mongohq.com:10073/node-api'
-    //'url' : 'localhost/conferenceDB'
+     //Uncomment to connect with MongoDB on Cloud
+    //'url' : 'mongodb://feiochc:hate666!@kahana.mongohq.com:10073/node-api'
+    'url' : 'mongodb://localhost/conferenceDB'
 };
